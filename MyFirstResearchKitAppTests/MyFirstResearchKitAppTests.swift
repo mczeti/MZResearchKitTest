86a4991343de676d27b1557caa2bd59ca7785da7
Martin Zehetner <mz@tailored-apps.com>
2015-06-26 15:37:21 +0200

    Initial commit

---
+++ MyFirstResearchKitAppTests/MyFirstResearchKitAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyFirstResearchKitAppTests.swift
+//  MyFirstResearchKitAppTests
+//
+//  Created by Martin Zehetner on 26.06.15.
+//  Copyright (c) 2015 Tailored Media GmbH. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MyFirstResearchKitAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
